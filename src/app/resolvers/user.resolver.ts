ecafdc6e3eafb5d4edf96e85c45ad930984947db
Anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2021-01-24 15:12:45 +0100

    Redirijo a una url donde se muestra el perfil y depiendiendo si es el que se ha logueado ira a /me o /users/id

--- src/app/resolvers/user.resolver.ts
+++ src/app/resolvers/user.resolver.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
 import { NEVER, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -15,7 +15,10 @@ export class UserResolver implements Resolve<User> {
   constructor(private userService: UserService, private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<User> | Observable<never> {
-    return route.params.id?this.userService.getProfile(route.params.id).pipe(
+    let urlpartfinal:UrlSegment = route.url[route.url.length-1];
+
+
+    return route.params.id || urlpartfinal.path==='me' ?this.userService.getProfile(route.params.id).pipe(
       catchError(() => {
         this.router.navigate(['/products']);
         return this.userService.getProfile();
